6b0067b35c5022c3aabdecd1728fe31982c0fdef
Moddy-t <amalembamodesta@gmail.com>
2024-07-21 16:50:52 +0300

    target sum lab

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set();
+  for (const number of array) {
+    const complement = target - number;
+    if (seenNumbers.has(complement)) {
+      return true; // If we have, return true
+    }
+    seenNumbers.add(number);
+  }
+  
+  // If we finish the loop without finding a pair, return false
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
